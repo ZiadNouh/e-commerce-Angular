a0b9ceb9e6052e65356635499e01f8b8ac08ca54
ziad <ziad.nouh111@gmail.com>
2024-07-25 18:32:23 +0300

    linked the json file data with the prods-List and displayed prod-card with static data

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import productsData from '../../assets/products.json';
+import { ProductCardComponent } from '../product-card/product-card.component';
 
 @Component({
   selector: 'app-products-list',
   standalone: true,
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './products-list.component.html',
-  styleUrl: './products-list.component.css'
+  styleUrl: './products-list.component.css',
 })
 export class ProductsListComponent {
-
+  products: Array<any> = productsData;
 }
