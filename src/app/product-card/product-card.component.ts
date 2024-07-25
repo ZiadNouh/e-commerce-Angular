dcc9dc7a8bbf55262333d05e2d5b6f81ce654b51
ziad <ziad.nouh111@gmail.com>
2024-07-25 20:01:06 +0300

    added product-list component functionality and Products Interface

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from '@angular/core';
+import { Product } from '../interface/Product';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css',
 })
 export class ProductCardComponent {
-  @Input() product: any;
+  @Input() product!: Product;
 }
