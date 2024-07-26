d63d537268d21f859d277454219e55d47718e251
ziad <ziad.nouh111@gmail.com>
2024-07-26 22:13:47 +0300

    completed loginform validation and style

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  formError: string = '';
 
+  handleLoginSubmit(loginForm: NgForm) {
+    if (loginForm.valid) {
+    } else {
+      const controls = loginForm.form.controls;
+
+      Object.keys(controls).forEach((key) => {
+        const control = controls[key];
+        control.markAsTouched();
+      });
+
+      this.formError =
+        'All fields must be completed correctly before submitting the form.';
+      console.log('Form is invalid');
+    }
+  }
 }
