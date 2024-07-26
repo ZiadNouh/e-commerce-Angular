cb93a8a692d9f1de9b81f8202be5890ff9167568
ziad <ziad.nouh111@gmail.com>
2024-07-26 09:55:33 +0300

    basic styling for the card-details

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import productsData from '../../assets/products.json';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
   imports: [],
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.css'
+  styleUrl: './product-details.component.css',
 })
 export class ProductDetailsComponent {
+  products: Array<any> = productsData;
+  id!: number;
+  productDetails: any;
 
+  constructor(private activateRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.id = +this.activateRoute.snapshot.params['id'];
+    console.log(this.id);
+    this.productDetails = this.products.find(
+      (product) => product.id === this.id
+    );
+    console.log(this.productDetails);
+  }
 }
