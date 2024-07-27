18bbabb0dd9f346f60c74b4950b368d9b4a84f96
ziad <ziad.nouh111@gmail.com>
2024-07-27 16:15:22 +0300

    added the html and register of the register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  NgForm,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  gameForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.gameForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators],
+    });
+  }
 
+  formError: string = '';
+  handleRegisterSubmit(registerForm: NgForm) {
+    console.log(registerForm.form.controls);
+  }
 }
