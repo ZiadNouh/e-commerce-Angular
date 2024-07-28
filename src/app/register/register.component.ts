69a727b38c0acc06a4e4cd68382651dd2eed851f
ziad <ziad.nouh111@gmail.com>
2024-07-28 14:02:48 +0300

    added regex for password and costume validation for confirm password

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -18,13 +18,45 @@ export class RegisterComponent {
   registerForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
-    this.registerForm = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', Validators.required],
-      userName: ['', Validators.required],
-      password: ['', Validators.required, Validators.minLength(4)],
-      confirmPassword: ['', Validators.required],
-    });
+    this.registerForm = this.fb.group(
+      {
+        name: ['', Validators.required],
+        email: ['', Validators.required, Validators.email],
+        userName: [
+          '',
+          Validators.required,
+          Validators.minLength(8),
+          Validators.pattern(/^\S*$/),
+        ],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(8),
+            Validators.pattern(
+              /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+            ),
+          ],
+        ],
+        confirmPassword: ['', Validators.required],
+      },
+      { validator: this.passwordMatchValidator }
+    );
+  }
+
+  passwordMatchValidator(form: FormGroup) {
+    const password = form.get('password');
+    const confirmPassword = form.get('confirmPassword');
+
+    if (
+      password &&
+      confirmPassword &&
+      password.value !== confirmPassword.value
+    ) {
+      confirmPassword.setErrors({ mustMatch: true });
+    } else {
+      confirmPassword?.setErrors(null);
+    }
   }
 
   get name() {
@@ -50,5 +82,19 @@ export class RegisterComponent {
   formError: string = '';
   handleRegisterSubmit() {
     console.log(this.registerForm.controls);
+
+    if (this.registerForm.valid) {
+    } else {
+      const controls = this.registerForm.controls;
+
+      Object.keys(controls).forEach((key) => {
+        const control = controls[key];
+        control.markAsTouched();
+      });
+
+      this.formError =
+        'All fields must be completed correctly before submitting the form.';
+      console.log('Form is invalid');
+    }
   }
 }
